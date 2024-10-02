a1e79ad7879db955b66c24ffa8f86335ef38d8fe
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-10-02 11:15:50 +0530

    coin deatil page added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
+import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const { setCurrency } = useContext(CoinContext)
+
+    const currencyHandler = (event) => {
+        switch (event.target.value) {
+            case "usd": {
+                setCurrency({ name: "usd", symbol: "$" });
+                break;
+            }
+            case "euro": {
+                setCurrency({ name: "euro", symbol: "€" })
+                break;
+            }
+            case "inr": {
+                setCurrency({ name: "inr", symbol: "₹" })
+                break;
+            }
+            default: {
+                setCurrency({ name: "usd", symbol: "$" })
+                break;
+            }
+
+        }
+    }
     return (
         <div>
             <div className="navbar">
-                <img src="https://yt3.googleusercontent.com/QAp7ArEFVNS_mvhDDXwz9VSygSFVET0-UQ6hEsGbagVA8qvMNQIMOe8NwJQM9BeDgkJZdMf6TQ=s900-c-k-c0x00ffffff-no-rj" alt="logo" className='logo'/>
+                <Link to={'/'}>
+                    <img src="https://yt3.googleusercontent.com/QAp7ArEFVNS_mvhDDXwz9VSygSFVET0-UQ6hEsGbagVA8qvMNQIMOe8NwJQM9BeDgkJZdMf6TQ=s900-c-k-c0x00ffffff-no-rj" alt="logo" className='logo' />
+                </Link>
                 <h1 className='head'>
-                Cryptify   
-                <span className='tail'>Track</span>
+                    Cryptify
+                    <span className='tail'>Track</span>
                 </h1>
                 <ul>
-                    <li>Home</li>
+                    <Link to={'/'}>
+                        <li>Home</li>
+                    </Link>
                     <li>Features</li>
                     <li>Pricing</li>
                     <li>Blog</li>
                 </ul>
                 <div className="nav-right">
-                    <select>
+                    <select onChange={currencyHandler}>
                         <option value="usd">USD</option>
                         <option value="eur">EURO</option>
                         <option value="inr">INR</option>
                     </select>
-                    <button >Sign up 
+                    <button >Sign up
                     </button>
                 </div>
             </div>
--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Coin.css'
-
+import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../context/CoinContext';
+import Linechart from '../../components/LineChart/Linechart';
 
 const Coin = () => {
-  return (
-    <div>
-      coin
-    </div>
-  )
+
+  const { coinId } = useParams();
+
+  const [coinData, setCoinData] = useState()
+  const [historicalData, setHistoricalData] = useState()
+  const { currency } = useContext(CoinContext)
+
+  const fetchCoinData = async () => {
+    const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(response => response.json())
+      .then(response => setCoinData(response))
+      .catch(err => console.error(err));
+  }
+
+
+
+  const fetchHistoricalData = async () => {
+    const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options)
+      .then(response => response.json())
+      .then(response => setHistoricalData(response))
+      .catch(err => console.error(err));
+  }
+
+
+
+  useEffect(() => {
+    fetchCoinData();
+    fetchHistoricalData();
+  }, [currency])
+
+  if (coinData && historicalData) {
+    return (
+      <div className='coin'>
+        <div className="coin-name">
+          <img src={coinData.image.large} alt="" />
+          <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+        </div>
+        <div className="coin-chart">
+          <Linechart historicalData={historicalData}/>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className='spinner'>
+        <div className="spin">
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Coin
